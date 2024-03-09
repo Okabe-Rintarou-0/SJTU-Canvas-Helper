e3cb3c8e340bceda87653d37e739f0c9c8a652bb
923048992@qq.com <923048992@qq.com>
2024-03-09 13:50:00 +0800

    fix: dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import AppRouter from "./components/router";
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
+import { checkUpdate, installUpdate, } from "@tauri-apps/api/updater";
+import { useEffect } from "react";
+
 function App() {
+  const checkAndUpdate = async () => {
+    const update = await checkUpdate();
+    if (update.shouldUpdate) {
+      await installUpdate();
+    }
+  }
+
+  useEffect(() => {
+    checkAndUpdate();
+  }, []);
+
   return <ConfigProvider locale={zhCN}>
     <AppRouter />
   </ConfigProvider>
