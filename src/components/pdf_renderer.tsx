686a46bc8b62e00b902902bd7b40ccbef7533ccb
923048992@qq.com <923048992@qq.com>
2025-12-27 22:42:17 +0800

    chore: optimize frontend code

--- src/components/pdf_renderer.tsx
+++ src/components/pdf_renderer.tsx
@@ -1,19 +1,31 @@
 import { DocRendererProps } from "@cyntler/react-doc-viewer";
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import "react-ipynb-renderer/dist/styles/darkbronco.css";
+import { Card } from "antd";
 import { base64ToBuffer, getBase64Data } from "../lib/utils";
-
 
 export default function PdfRenderer(props: DocRendererProps) {
     let currentDocument = props.mainState.currentDocument;
     if (!currentDocument || currentDocument.fileData === undefined) return null;
     const data = base64ToBuffer(getBase64Data(currentDocument.fileData as string));
-    return <div style={{
-        minWidth: 1080
-    }}>
-        <Viewer fileUrl={data} />
-    </div>
+    return (
+        <Card style={{
+            width: "100%",
+            height: "100%",
+            minHeight: 600,
+            borderRadius: 8,
+            boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+            overflow: "scroll"
+        }}>
+            <div style={{
+                width: "100%",
+                height: "calc(100% - 24px)",
+                overflow: "auto"
+            }}>
+                <Viewer fileUrl={data} />
+            </div>
+        </Card>
+    );
 }
 
 PdfRenderer.fileTypes = ['pdf'];
