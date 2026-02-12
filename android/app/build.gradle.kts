5140fb33cb609a28236aa09807dee614a2ef9153
liu <62530004+xeonliu@users.noreply.github.com>
2026-02-12 12:23:28 +0800

    feat: Add okhttp-urlconnection dependency for enhanced networking support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.12.0")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
